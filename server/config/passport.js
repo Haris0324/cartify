d4868a598f6eca73c6ef368e0f683418dd5d1337
Haris0324 <harislaeeq2403@gmail.com>
2026-03-14 18:19:20 +0500

    Fix github OAuth callback

--- server/config/passport.js
+++ server/config/passport.js
@@ -45,45 +45,43 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
 }
 
 if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
-  passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID || '',
-    clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-    callbackURL: `${process.env.API_URL || 'http://localhost:5000'}/api/auth/github/callback`,
+    passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://13.53.39.196:5000/api/auth/github/callback",
     scope: ['user:email']
-  }, async (accessToken, refreshToken, profile, done) => {
-
+    }, async (accessToken, refreshToken, profile, done) => {
     try {
-      const email = profile.emails?.[0]?.value || `${profile.id}@github.user`;
+
+      const email =
+        profile.emails?.[0]?.value ||
+        `${profile.username}@github.user`;
 
       let user = await User.findOne({ githubId: profile.id });
+
       if (user) return done(null, user);
 
       user = await User.findOne({ email });
 
       if (user) {
         user.githubId = profile.id;
-
-        if (!user.avatar && profile.photos?.[0]?.value) {
-          user.avatar = profile.photos[0].value;
-        }
-
         await user.save();
         return done(null, user);
       }
 
       user = await User.create({
-        name: profile.displayName || profile.username || email.split('@')[0],
+        name: profile.displayName || profile.username,
         email,
         githubId: profile.id,
-        avatar: profile.photos?.[0]?.value || ''
+        avatar: profile.photos?.[0]?.value || ""
       });
 
-      return done(null, user);
+      done(null, user);
 
     } catch (err) {
-      return done(err, null);
+      done(err, null);
     }
-  }));
+    }));
 }
 
 passport.serializeUser((user, done) => done(null, user._id));
