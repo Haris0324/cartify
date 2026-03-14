54f90c6e63d222cfbd05aeff986b2628b4fb79ff
Haris0324 <harislaeeq2403@gmail.com>
2026-03-14 17:58:56 +0500

    Fix OAuth callback url

--- server/config/passport.js
+++ server/config/passport.js
@@ -3,14 +3,17 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const GitHubStrategy = require('passport-github2').Strategy;
 const User = require('../models/User');
 
+const BASE_URL = "http://13.53.39.196:5000";
+
 if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
   passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID || '',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-    callbackURL: `${process.env.API_URL || 'http://localhost:5000'}/api/auth/google/callback`
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${BASE_URL}/api/auth/google/callback`
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       const email = profile.emails?.[0]?.value;
+
       let user = await User.findOne({ googleId: profile.id });
       if (user) return done(null, user);
 
@@ -18,7 +21,9 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
         user = await User.findOne({ email });
         if (user) {
           user.googleId = profile.id;
-          if (!user.avatar && profile.photos?.[0]?.value) user.avatar = profile.photos[0].value;
+          if (!user.avatar && profile.photos?.[0]?.value) {
+            user.avatar = profile.photos[0].value;
+          }
           await user.save();
           return done(null, user);
         }
@@ -30,7 +35,9 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
         googleId: profile.id,
         avatar: profile.photos?.[0]?.value || ''
       });
+
       return done(null, user);
+
     } catch (err) {
       return done(err, null);
     }
@@ -39,19 +46,26 @@ if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
 
 if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
   passport.use(new GitHubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID || '',
-    clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
-    callbackURL: `${process.env.API_URL || 'http://localhost:5000'}/api/auth/github/callback`
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: `${BASE_URL}/api/auth/github/callback`
   }, async (accessToken, refreshToken, profile, done) => {
+
     try {
       const email = profile.emails?.[0]?.value || `${profile.id}@github.user`;
+
       let user = await User.findOne({ githubId: profile.id });
       if (user) return done(null, user);
 
       user = await User.findOne({ email });
+
       if (user) {
         user.githubId = profile.id;
-        if (!user.avatar && profile.photos?.[0]?.value) user.avatar = profile.photos[0].value;
+
+        if (!user.avatar && profile.photos?.[0]?.value) {
+          user.avatar = profile.photos[0].value;
+        }
+
         await user.save();
         return done(null, user);
       }
@@ -62,7 +76,9 @@ if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
         githubId: profile.id,
         avatar: profile.photos?.[0]?.value || ''
       });
+
       return done(null, user);
+
     } catch (err) {
       return done(err, null);
     }
@@ -70,6 +86,7 @@ if (process.env.GITHUB_CLIENT_ID && process.env.GITHUB_CLIENT_SECRET) {
 }
 
 passport.serializeUser((user, done) => done(null, user._id));
+
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
